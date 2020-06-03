49c11662e4d93309febbc1106b097a0d45dc1827
“qiuyahui <qyh0518@163.com>
2020-06-03 18:29:09 +0800

    2020-6-3

--- src/views/transition/index.vue
+++ src/views/transition/index.vue
@@ -4,6 +4,8 @@
       <div class="test" />
     </div>
     <button @click="start">动画</button>
+
+    <div class="anniu" />
   </div>
 </template>
 
@@ -39,6 +41,15 @@ export default {
         width:373px;
         transition: width 3s linear;
         transform-origin: 100% 0;
+    }
+    .anniu{
+        margin: 50px;
+        width: 200px;
+        height: 200px;
+        border-radius: 44px;
+        background: #71d7bf;
+        box-shadow:  10px 10px 20px #458375,
+             -10px -10px 20px #9dffff;
     }
 </style>>
 
